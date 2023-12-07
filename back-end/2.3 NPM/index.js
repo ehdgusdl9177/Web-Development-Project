317117bf9d85dddd1eb21c009109c65143673787
ehdgusdl9177 <k23129177@gmail.com>
2023-12-07 14:35:42 +0900

    NPM solution

---
+++ back-end/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from "sillyname";
+
+let sillyName = generateName();
+
+console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}`);
