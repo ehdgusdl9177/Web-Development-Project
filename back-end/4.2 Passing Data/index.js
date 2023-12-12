dfa3580205f8ced3b5c88b1809beedd183978379
ehdgusdl9177 <k23129177@gmail.com>
2023-12-12 17:21:21 +0900

    EJS passing data

--- back-end/4.2 Passing Data/index.js
+++ back-end/4.2 Passing Data/index.js
@@ -6,9 +6,15 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  const numLetters = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", { numberOfLetter: numLetters });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
