b4cb2b534bd376bee7f6a86a87c006f450548b48
ehdgusdl9177 <k23129177@gmail.com>
2023-12-28 11:28:39 +0900

    delete all joke

--- back-end/6.0 DIY API/index.js
+++ back-end/6.0 DIY API/index.js
@@ -83,6 +83,17 @@ app.delete("/jokes/:id", (req, res) => {
 });
 
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(400)
+      .json({ error: `You are not authorised to perform this action` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
